24d9cc97beb909e3f3e8209a28f21abbac8fa1ed
Viacheslav <iexploudi@gmail.com>
2025-02-19 22:28:51 +0400

    Create some classes Person and Employee

---
+++ src/main/java/inheritance/Employee.java
@@ -0,0 +1,22 @@
+package inheritance;
+
+public class Employee extends Person{
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    private String employeeId;
+    private String title;
+}
