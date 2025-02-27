e2d61a3d52cb886aa4ba0230ebd5a1d7c87eb28b
Viacheslav <iexploudi@gmail.com>
2025-02-27 22:30:03 +0400

    create abstract class

---
+++ src/main/java/abstraction/Shape.java
@@ -0,0 +1,14 @@
+package abstraction;
+
+public abstract sealed class Shape  permits Rectangle{
+    /**
+     * - abstract classes and methods are templates
+     * - declare using abstact reserved word
+     * - subclasses must implement inherited abstract methods
+     */
+    abstract double calculateArea();
+
+    public void print(){
+        System.out.println("I am a shape");
+    }
+}
---
+++ src/main/java/abstraction/ShapeTester.java
@@ -0,0 +1,10 @@
+package abstraction;
+
+public class ShapeTester {
+
+    public static void main(String[] args) {
+        Shape rectangle = new Rectangle(5, 6);
+        rectangle.print();
+        System.out.println(rectangle.calculateArea());
+    }
+}
