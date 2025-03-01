efc61f9761ab91e7b810dde6b161cbc4cc082a7e
Viacheslav <iexploudi@gmail.com>
2025-03-01 12:25:37 +0400

    interfaces and info about it

---
+++ src/main/java/interfaces/Product.java
@@ -0,0 +1,11 @@
+package interfaces;
+
+public interface Product {
+    /**
+     * types of Methods in Interfaces
+     * - abstract; - default; - static;
+     */
+    String getName();
+    void setName(String name);
+
+}
