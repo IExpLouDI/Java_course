ac367e75b68c9be015f8fa333b5a67ffb86e372f
Viacheslav <iexploudi@gmail.com>
2025-03-01 20:05:37 +0400

    Work with list

--- src/main/java/data_structure/CollectionsDemo.java
+++ src/main/java/data_structure/CollectionsDemo.java
@@ -1,12 +1,40 @@
 package data_structure;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class CollectionsDemo {
     public static void main(String[] args) {
-        setDemo();
+//        setDemo();
+        listDemo();
     }
+
+    public static void listDemo(){
+        List fruits = new ArrayList();
+        fruits.add("apple");
+        fruits.add("lemon");
+        fruits.add("banana");
+        fruits.add("orange");
+
+        fruits.add("apple");
+        fruits.set(2, "grape");
+        fruits.remove("lemon");
+//        fruits.remove(3);
+
+        System.out.println("Index 2: " + fruits.get(2));
+        System.out.println("Index of grape: " + fruits.indexOf("grape"));
+        System.out.println("Last index of lemon: " + fruits.lastIndexOf("apple"));
+
+        System.out.println(fruits);
+
+        List moreFruits = List.of("cherry", "plum"); //immutable
+
+        System.out.println(moreFruits);
+
+    }
+
     public static void setDemo(){
         Set fruits = new HashSet();
         fruits.add("apple");
